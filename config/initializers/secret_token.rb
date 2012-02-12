4df203598ac834c14f55110dce56bffd7d94d913
Andrii Oleksyshyn <orphy@ukr.net>
2012-02-12 22:13:08 +0200

    Comments

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrphyApp::Application.config.secret_token = 'bf09c93c679dac981be5d282897993363074d71bf1eb87a30317f6c867a9ad147100addaaa2e9fe8821037f16e078b40f04cab73eb5d66c57e3dc7bca8644167'
